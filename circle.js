06302be393d91c0b59aa474719e63dffffa11d1e
Naji <najismitanaik@gmail.com>
2026-05-04 16:28:16 +0530

    third commit

--- circle.js
+++ circle.js
@@ -5,3 +5,5 @@ function circumference(radius) {
 // Example usage
 console.log(circumference(5)); // Output: 31.41592653589793
 //i need to change something
+// This function calculates the area of a circle given its radius
+function area(radius) {
